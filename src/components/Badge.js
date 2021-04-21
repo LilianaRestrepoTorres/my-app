474453d27826626c77fb5f51fcec1cb90980de9e
Liliana Restrepo <Lili@MacBook-Pro-de-Liliana.local>
2021-04-21 05:36:05 -0500

    Add styles to component

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,26 +1,28 @@
 import React from 'react';
 
+import "./styles/Badge.css";
 import confLogo from '../images/badge-header.svg';
+
 
 class Badge extends React.Component{
     render(){
         return (
-            <div>
-                <div>
+            <div className="Badge">
+                <div className="Badge__header">
                     <img src={confLogo} alt="Logo de la conferencia"></img>
                 </div>
 
-                <div>
-                    <img src="" alt="Avatar"></img>
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="https://s.gravatar.com/avatar/da73277848b527cfa25ed0e5cce89a33?s=80" alt="Avatar"></img>
                     <h1>Liliana <br/> Restrepo</h1>
                 </div>
 
-                <div>
+                <div className="Badge__section-info">
                     <p>Fronted Engineer</p>
                     <p>@lilirestrepot</p>
                 </div>
 
-                <div>
+                <div className="Badge__footer">
                     #platziconf
                 </div>
             </div>
