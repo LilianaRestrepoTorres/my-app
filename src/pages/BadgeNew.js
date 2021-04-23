08eb83f5a20de4df243c56d14f0b7389f4166a75
Liliana Restrepo <Lili@MacBook-Pro-de-Liliana.local>
2021-04-23 07:34:51 -0500

    The folder pages is added with the structure that contains the components

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -3,6 +3,7 @@ import React from 'react';
 import  './styles/BadgeNew.css'
 import header from '../images/badge-header.svg';
 import Navbar from '../components/Navbar';
+import Badge from '../components/Badge';
 
 class BadgeNew extends React.Component{
     render(){
@@ -12,6 +13,20 @@ class BadgeNew extends React.Component{
                 <div className="BadgeNew__hero">
                     <img className="img-fluid" src={header} alt="Logo"/>
 
+                </div>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <Badge 
+                                firstName = "Liliana"
+                                lastName = "Restrepo"
+                                twitter ="lilirestrepot"
+                                jobTitle ="Frontend Engeneer"
+                                avatarUrl = "https://s.gravatar.com/avatar/da73277848b527cfa25ed0e5cce89a33?s=80"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         )
