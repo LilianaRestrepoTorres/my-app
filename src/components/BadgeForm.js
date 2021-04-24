0c334efb8f29b38f517c7cd5ea7ba259a69de993
Liliana Restrepo <Lili@MacBook-Pro-de-Liliana.local>
2021-04-23 21:07:17 -0500

    Linking events

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class BadgeForm extends React.Component{
     handleChange = (e) => {
-        console.log({ value: e.target.value });
+        console.log({ 
+            name: e.target.name,
+            value: e.target.value
+        });
+    }
+
+    handleClick = (e) =>{
+        console.log('Button was clicked');
+    }
+
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log("Form was submited")
     }
 
     render(){
@@ -10,13 +22,15 @@ class BadgeForm extends React.Component{
             <div>
                 <h1>New Attendant</h1>
 
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label>First Name</label>
                         <input onChange={this.handleChange} className="form-control" type="text" name="firstName"/>
 
                     </div>
-                    <button className="btn btn-primary">Save</button>
+                    <button onClick={this.handleClick} className="btn btn-primary">
+                        Save
+                    </button>
 
                 </form>
             </div>
